a229d15a58b63bf28ac037fa83c63c4a0d83a4fe
Devyani Gotiwale <156320335+GotiwaleDevyani@users.noreply.github.com>
2024-01-30 11:04:34 +0530

    Add files via upload

---
+++ MultiLevel.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+class MultiLevel
+{
+    public static void main(String A[])
+    {
+        DerivedX dobj = new DerivedX();
+        
+        dobj.fun();
+        dobj.gun();
+    }
+}
+
+class Base
+{
+    public int A,B;
+
+    public Base()
+    {
+         System.out.println("Base Constructor");
+    }
+    public void fun()
+    {
+         System.out.println("Base fun");
+    }
+    
+}
+class Derived extends Base
+{
+    public int X,Y;
+
+    public Derived()
+    {
+         System.out.println("Derived Constructor");
+    }
+    public void gun()
+    {
+         System.out.println("Derived gun");
+    }
+    
+}
+class DerivedX extends Derived
+{
+   public int P,Q;
+
+    public DerivedX()
+    {
+        System.out.println("DerivedX Constructor");
+    }
+}
---
+++ MultiThread6.java
@@ -0,0 +1,32 @@
+class MultiThread6
+{
+    public static void main(String Arg[]) throws Exception
+    {
+        System.out.println("Current thread is :"+Thread.currentThread().getName());
+        
+        Demo obj1 = new Demo(); //new state
+          
+        Demo obj2 = new Demo(); //new state          //Runnable state
+
+        //obj1.start();   //Runnable state //5
+        //obj2.start();     //Runnable state //5
+
+        //System.out.println("priority of obj1 is :"+obj1.getPriority());
+       //System.out.println("priority of obj2 is :"+obj2.getPriority());
+
+        obj1.setPriority(8); 
+        obj2.setPriority(10);
+        obj1.start();   //Runnable state   //8
+        obj2.start();     //Runnable state  //10
+        System.out.println("priority of obj1 is :"+obj1.getPriority());
+        System.out.println("priority of obj2 is :"+obj2.getPriority());
+    }
+    
+}
+class Demo extends Thread
+{
+    public void run() //Running state
+    {
+    
+    }
+}
