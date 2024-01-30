b290e1dd3fe6c6af84a54a9f0b56601a8fa45e22
Devyani Gotiwale <156320335+GotiwaleDevyani@users.noreply.github.com>
2024-01-30 11:03:07 +0530

    Add files via upload

---
+++ Interface.java
@@ -0,0 +1,15 @@
+ interface InterDemo1
+ {
+    void fun();
+ }
+ interface Hello
+ {
+    void gun();
+ }
+
+ class Marvellous implements Demo,Hello{
+    public void fun();
+    { }
+    public void fun();
+    {}
+ }
